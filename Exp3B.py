9093ba835b34d3cf7479bdc98a862bc99af2f8f1
Ignes <ignes@pm.me>
2019-09-12 17:59:56 +0530

    Update Exp3B.py

--- Exp3B.py
+++ Exp3B.py
@@ -1,16 +1,11 @@
 import string
 import random
 
-upper, lower, digits, symbols = 0, 0, 0, 0
+def gen():
+    data = string.ascii_letters+string.punctuation+string.digits
+    size = random.randint(8, 12)
+    return ''.join(random.choice(data) for x in range(size))
 
 
-def randompassword():
-    password = ''
-    chars = string.printable
-    chars = chars.replace('\n', string.digits)
-    size = random.randint(10, 16)
-    return password.join(random.choice(chars) for x in range(size))
-
-
-password = randompassword()
-print(f"Random password generated of length {len(password)}\n", password)
+password = gen()
+print(f"Random password generated of length {len(password)}\n",password)
