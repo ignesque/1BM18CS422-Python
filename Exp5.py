951ec82c6fb8c0f1d00a03cc8a6fa019a673c863
Ignes <ignes@pm.me>
2019-09-26 18:45:03 +0530

    Update Exp5.py

--- Exp5.py
+++ Exp5.py
@@ -1,32 +1,36 @@
 class CallDetail:
-    def __init__(self,calls):
-        self.calls=calls
-        self.fromcall,self.tocall,self.callspan,self.calltype=None,None,None,None
+
+    def __init__(self, source, dest, span, type):
+        self.callfrom = source
+        self.callto = dest
+        self.duration = span
+        self.calltype = type
+
     def printDetails(self):
-        for i in range(len(self.calls)):
-            print(f"Call Detail {i+1}:")
-            self.details=self.calls[i].split(",")
-            self.fromcall=self.details[0]
-            self.tocall = self.details[1]
-            self.callspan = self.details[2]
-            self.calltype = self.details[3]
-            print(f"Called from:{self.fromcall}\nCalled to:{self.tocall}\nCall Duration:{self.callspan}\nCall Type:{self.calltype}\n")
+       print(f"Call From:{self.callfrom}\nCall To:{self.callto}\nCall Duration:{self.duration}\nCall Type:{self.calltype}\n")
 
-class Util:
+class util:
     def __init__(self):
-        self.callobj = None
+        self.list_of_call_objects = []
 
-    def parseCustomer(self, callString):
-        self.callobj=[CallDetail(callString)]
-        return self.callobj
-    def details(self,callobj):
-        callobj[0].printDetails()
+    def parse_customer(self, list_of_call_string):
+        for calls in list_of_call_string:
+            details = calls.split(",")
+            obj = CallDetail(*details)
+            self.list_of_call_objects.append(obj)
+
+    def getDetails(self):
+        for obj in range(len(self.list_of_call_objects)):
+            print(f"Call Detail {obj+1}")
+            self.list_of_call_objects[obj].printDetails()
 
 
-call = '9990000001,9330000001,23,STD'
-call2 = '9990000001,9330000002,54,Local'
-call3 = '9990000001,9330000003,6,ISD'
-callString = [call, call2, call3]
-callobj=Util().parseCustomer(callString)
-Util().details(callobj)
+call='9990000001,9330000001,23,STD'
+call2='9990000001,9330000002,54,Local'
+call3='9990000001,9330000003,6,ISD'
 
+list_of_call_string = [call, call2, call3]
+util().parse_customer(list_of_call_string)
+ob = util()
+ob.parse_customer(list_of_call_string)
+ob.getDetails()
