9ae4c6eb1953bf96ba0bd18f0d3ce83d37849093
Ignes <ignes@pm.me>
2019-10-10 18:12:28 +0530

    Update Exp7B.py

--- Exp7B.py
+++ Exp7B.py
@@ -1,4 +1,10 @@
-for i in range(5):
-    print("  --- "*5)
-    print("|     "*6)
-print("  --- " * 5)
+def board(row, col):
+    for i in range(row):
+        print("  --- " * col)
+        print("|     " * (col + 1))
+    print("  --- " * col)
+
+
+row = int(input("Enter row size of board:"))
+col = int(input("Enter col size of board:"))
+board(row, col)
